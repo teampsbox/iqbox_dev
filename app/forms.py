39933eebfa9aeaf96911916e2cb921c08325b6eb
teampsbox <teampsbox@gmail.com>
2018-08-31 19:33:14 +0800

    Added Length validator in forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class QuoteForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    body = TextAreaField('Body', validators=[DataRequired()])
+    body = TextAreaField('Body', validators=[DataRequired(), Length(min=10, max=250)])
     by = StringField('By')
     submit = SubmitField('Share')
